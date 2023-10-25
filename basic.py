3ea5bd2d86002f8434f5365d7abad9d0c31f4633
Daniel Ochoa <71296521+Dopo0@users.noreply.github.com>
2023-10-25 13:16:32 -0400

    Update basic.py

--- basic.py
+++ basic.py
@@ -1,3 +1,11 @@
 import streamlit as st
+import torch
 
 st.title("Hello World")
+
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
+
+st.write("using", device, "device")
